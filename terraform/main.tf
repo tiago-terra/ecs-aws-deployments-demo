afdfcbca43eae740893e4ad5f43b1f5c031a1000
Tiago Terra <tiago.terra@ecs-digital.co.uk>
2020-10-14 19:02:56 +0200

    added pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,19 +6,9 @@ module "cloud_setup" {
   user_name = data.aws_iam_user.main.user_name
 }
 
-module "cloudbuild_build" {
+module "code_build" {
   source = "./modules/code_build"
 
-  stage = "build"
-  role_arn = data.aws_iam_role.main.arn
-  ecr_repo = module.cloud_setup.ecr_repo
-}
-
-
-module "cloudbuild_deploy" {
-  source = "./modules/code_build"
-
-  project_name = "codebuild-${var.project_name}"
   role_arn = data.aws_iam_role.main.arn
   ecr_repo = module.cloud_setup.ecr_repo
 }
