8cd2438bf36cecb78ce1bd76f6fcc58d9184c924
Tiago Terra <tiago.terra@ecs-digital.co.uk>
2020-10-16 22:24:05 +0200

    fixed nginx hmtl

--- codebuild.sh
+++ codebuild.sh
@@ -7,6 +7,9 @@ export KUBE_URL="https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-2
 export MANIFEST_PATH="k8s"
 if [ -z $1 ];then echo "Argument missing!\nUsage: $0 \$action" && exit 1; fi
 
+function exit_run () {
+    if [ "$CODEBUILD_BUILD_SUCCEEDING" == "0" ]; then echo "Error: exiting script..."; exit 1; fi
+}
 
 function build_push_ecr () {
   echo "test"
@@ -16,11 +19,10 @@ function build_push_ecr () {
   echo "Building docker image..."
   docker build -t $IMAGE_URI docker --build-arg IMAGE_TAG=$2
 
-  if [ "$CODEBUILD_BUILD_SUCCEEDING" == "0" ]; then exit 1; fi
-
   echo "Pushing image with tag :$1 to repo $2..."
   docker push $IMAGE_URI
-
+  exit_run
+  
   echo "Image pushed to ECR!"
 }
 
