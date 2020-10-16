ecd4a798af02f3815f26f3617764c163513a9f73
Tiago Terra <tiago.terra@ecs-digital.co.uk>
2020-10-16 22:02:24 +0200

    fixed nginx hmtl

--- terraform/cloud_setup/iam.tf
+++ terraform/cloud_setup/iam.tf
@@ -8,16 +8,36 @@ data "aws_iam_role" "main" {
   name = var.role_name
 }
 
+# Create EKSAdmin policy
+resource "aws_iam_policy" "eks_admin" {
+  name        = "EKSAdminPolicy"
+  path        = "/"
+  description = "EKS Administrator policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": "eks:*",
+          "Resource": "*"
+      }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_user_policy_attachment" "user_attachment" {
   count      = length(var.policies)
   user       = data.aws_iam_user.main.user_name
-  policy_arn = element(var.policies, count.index)
+  policy_arn = element(concat(var.policies,[aws_iam_policy.eks_admin.arn]), count.index)
 }
 
 resource "aws_iam_role_policy_attachment" "role_policy" {
   count      = length(var.policies)
   role       = data.aws_iam_role.main.name
-  policy_arn = element(var.policies, count.index)
+  policy_arn = element(concat(var.policies,[aws_iam_policy.eks_admin.arn]), count.index)
 }
 
 # IAM User - attach public key
