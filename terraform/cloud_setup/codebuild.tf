4f0c01d74575b4153193e3497b2e2c8611552035
Tiago Terra <tiago.terra@ecs-digital.co.uk>
2020-10-17 15:57:31 +0200

    added kubectl provisioning to tf

--- terraform/cloud_setup/codebuild.tf
+++ terraform/cloud_setup/codebuild.tf
@@ -36,6 +36,11 @@ resource "aws_codebuild_project" "main" {
       name = "EKS_CLUSTER_NAME"
       value = aws_eks_cluster.main.name
     }
+    environment_variable {
+      name = "DEPLOY_ROLE_ARN"
+      value = aws_iam_role.deploy_role.arn
+    }
+    
   }
   source {
     type      = "CODEPIPELINE"
